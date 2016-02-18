c34c181808e0c65591aab741e9441883c4163baf
robin890650 <robin890650@gmail.com>
2016-02-18 22:34:55 +0800

    Add another sample data 1M

--- src/main/java/data/privacy/experiments/multiy/Fakedata.java
+++ src/main/java/data/privacy/experiments/multiy/Fakedata.java
@@ -17,17 +17,17 @@ public class Fakedata {
 		Random rng = new Random();
 		double epsilon = 0.8;
 
-		Domain domain = new Domain(RESOURCE_PATH + "fakedata.domain.csv");
-		Data gTrain = new Data(RESOURCE_PATH + "fakedata.csv", domain);
+		Domain domain = new Domain(RESOURCE_PATH + "fakedata2.domain.csv");
+		Data gTrain = new Data(RESOURCE_PATH + "fakedata2.csv", domain);
 		//
 //		gTrain.printo_int("result/fakedata-coarse.csv", ",");
 		Data bTrain = gTrain.binarization();
 
-		Bayesian bayes = new Bayesian(rng, bTrain, epsilon, 0.5, 4.0, 3);
+		Bayesian bayes = new Bayesian(rng, bTrain, epsilon, 0.5, 4.0, 2);
 		bayes.release().generalization()
-				.printo_int("result/FakeSyntheticDataTrans.csv", ",");
+				.printo_int("result/Fake2SyntheticDataTrans.csv", ",");
 		bayes.release().generalization()
-				.printo_data("result/FakeSyntheticData.csv", ",");
+				.printo_data("result/Fake2SyntheticData.csv", ",");
 		
 		TimeBottle.showAverage();
 	}
